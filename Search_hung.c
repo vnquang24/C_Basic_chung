6926d9da8a982524e18db4f52d99286124ae5425
HexDino <129679654+HexDino@users.noreply.github.com>
2023-11-29 18:39:55 +0700

    Code Search cho dslk

--- Search_hung.c
+++ Search_hung.c
@@ -30,39 +30,18 @@ void addNode(node_t **head, phoneAddr_t data) {
     }
 }
 
-void moveToFront(node_t **head, node_t *prevNode, node_t *curNode) {
-    if (prevNode != NULL) {
-        prevNode->next = curNode->next;
-        curNode->next = *head;
-        *head = curNode;
-    }
-}
-
-void searchNode(node_t **head, char key[]) {
-    node_t *temp = *head;
-    node_t *prev = NULL;
+void searchNode(node_t *head, char key[]) {
+    node_t *temp = head;
     int index = 0;
     while (temp != NULL) {
         if (strcmp(temp->data.name, key) == 0) {
             printf("%d %s %s %s\n", index, temp->data.name, temp->data.tel, temp->data.email);
-            moveToFront(head, prev, temp);
             return;
         }
-        prev = temp;
         temp = temp->next;
         index++;
     }
     printf("Not found\n");
-}
-
-void printList(node_t *head) {
-    node_t *temp = head;
-    int index = 0;
-    while (temp != NULL) {
-        printf("%s %s %s\n", temp->data.name, temp->data.tel, temp->data.email);
-        temp = temp->next;
-        index++;
-    }
 }
 
 int main() {
@@ -78,8 +57,7 @@ int main() {
     }
 
     scanf("%s", key);
-    searchNode(&head, key);
-    printList(head);
+    searchNode(head, key);
 
     // Free memory
     node_t *temp;
